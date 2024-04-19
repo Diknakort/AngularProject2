ea85f6e7b1d5d338bd25d26a9120d0171e1f4dcc
Diknakort <diknakort@gmail.com>
2024-04-19 13:46:29 +0200

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AngularProject2';
+  title = 'mdf';
+  resultado = 0;
+  texto = "";
+  cadena = "";
+  composicion = "";
+  contactForm = new FormGroup({
+    nombre: new FormControl(),
+    peso: new FormControl(),
+    altura: new FormControl(),
+    sistema: new FormControl()
+  })
+  onSubmit() {
+    let pesoValor = Number(this.contactForm.value.peso);
+    let alturaValor = Number(this.contactForm.value.altura);
+    this.resultado = pesoValor / ((alturaValor * alturaValor) / 10000);
+    if (this.resultado < 18.5) {
+      this.composicion = "Peso inferior al normal";
+    } if (this.resultado < 24.9) {
+      this.composicion = "Normal";
+    } if (this.resultado < 29.9) {
+      this.composicion = "Peso superior al normal";
+    } else this.composicion = "Obesidad";
+
+    return `Hola ${this.contactForm.value.nombre}, su sistema de medida es ${this.contactForm.value.sistema},
+     tiene un peso: ${this.contactForm.value.peso} y de altura ${this.contactForm.value.altura}, un IMC de ${this.resultado}
+    y por tanto, usted tiene un IMC ${this.composicion}`;
+  }
+  dameResultado() {
+    let pesoValor = Number(this.contactForm.value.peso);
+    let alturaValor = Number(this.contactForm.value.altura);
+    this.resultado = pesoValor / ((alturaValor * alturaValor) / 10000);
+    let cadena: string = "";
+    if (this.resultado < 18.5) {
+      this.composicion = "Peso inferior al normal";
+    } if (this.resultado < 24.9) {
+      this.composicion = "Normal";
+    } if (this.resultado < 29.9) {
+      this.composicion = "Peso superior al normal";
+    } else this.composicion = "Obesidad";
+
+    return `Hola ${this.contactForm.value.nombre}, su sistema de medida es ${this.contactForm.value.sistema},
+     tiene un peso: ${this.contactForm.value.peso} y de altura ${this.contactForm.value.altura}, un IMC de ${this.resultado}
+     y por tanto, usted tiene un IMC ${this.composicion}`;
+  }
 }
+
+
